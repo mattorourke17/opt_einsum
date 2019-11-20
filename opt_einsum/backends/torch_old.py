97de321bdbbeaf10b4808a0d2e1a1ca38655092d
matt o'rourke <mattorourke41@gmail.com>
2019-11-19 23:17:46 -0800

    trying to support saving to disk for pytorch contraction

--- opt_einsum/backends/torch_old.py
+++ opt_einsum/backends/torch_old.py
@@ -6,16 +6,11 @@ import numpy as np
 
 from ..parser import convert_to_valid_einsum_chars
 from ..sharing import to_backend_cache_wrap
-from ..torch_ad_helper.tensordot import Tensordot as TDOT
 
-__all__ = ["transpose", "tensordot", "to_torch", "build_expression", "evaluate_constants"]
+__all__ = ["transpose", "einsum", "tensordot", "to_torch", "build_expression", "evaluate_constants"]
 
 _TORCH_DEVICE = None
 _TORCH_HAS_TENSORDOT = None
-
-#save tensordot intermediates to disk (True) or try to store them all in RAM (False)
-global TDOT_TO_DISK
-TDOT_TO_DISK = True
 
 _torch_symbols_base = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -32,13 +27,6 @@ def _get_torch_and_device():
 
     return _TORCH_DEVICE
 
-# wrap the custom tensordot class
-def diff_tdot(A, B, axes):
-    if TDOT_TO_DISK:
-        return TDOT.apply(A, B, axes, True)
-    else:
-        return TDOT.apply(A, B, axes, False) 
-
 
 def transpose(a, axes):
     """Normal torch transpose is only valid for 2D matrices.
@@ -46,11 +34,65 @@ def transpose(a, axes):
     return a.permute(*axes)
 
 
+def einsum(equation, *operands):
+    """Variadic version of torch.einsum to match numpy api.
+    """
+    # rename symbols to support PyTorch 0.4.1 and earlier,
+    # which allow only symbols a-z.
+    equation = convert_to_valid_einsum_chars(equation)
+
+    torch, _ = _get_torch_and_device()
+    return torch.einsum(equation, operands)
+
 
 def tensordot(x, y, axes=2):
     """Simple translation of tensordot syntax to einsum.
     """
-    return diff_tdot(x, y, axes)
+    torch, _ = _get_torch_and_device()
+
+    if _TORCH_HAS_TENSORDOT:
+        return torch.tensordot(x, y, dims=axes)
+
+    xnd = x.ndimension()
+    ynd = y.ndimension()
+
+    # convert int argument to (list[int], list[int])
+    if isinstance(axes, int):
+        axes = range(xnd - axes, xnd), range(axes)
+
+    # convert (int, int) to (list[int], list[int])
+    if isinstance(axes[0], int):
+        axes = (axes[0],), axes[1]
+    if isinstance(axes[1], int):
+        axes = axes[0], (axes[1],)
+
+    # initialize empty indices
+    x_ix = [None] * xnd
+    y_ix = [None] * ynd
+    out_ix = []
+
+    # fill in repeated indices
+    available_ix = iter(_torch_symbols_base)
+    for ax1, ax2 in zip(*axes):
+        repeat = next(available_ix)
+        x_ix[ax1] = repeat
+        y_ix[ax2] = repeat
+
+    # fill in the rest, and maintain output order
+    for i in range(xnd):
+        if x_ix[i] is None:
+            leave = next(available_ix)
+            x_ix[i] = leave
+            out_ix.append(leave)
+    for i in range(ynd):
+        if y_ix[i] is None:
+            leave = next(available_ix)
+            y_ix[i] = leave
+            out_ix.append(leave)
+
+    # form full string and contract!
+    einsum_str = "{},{}->{}".format(*map("".join, (x_ix, y_ix, out_ix)))
+    return einsum(einsum_str, x, y)
 
 
 @to_backend_cache_wrap
---
+++ opt_einsum/torch_ad_helper/tensordot.py
@@ -0,0 +1,108 @@
+# Author: Matt O'Rourke
+# Custom differentiable tensordot implementation to allow for disk storage of
+# intermediates. This code relies heavily on the concepts from the HIPS/autograd
+# implementation (github.com/HIPS/autograd).
+
+
+import numpy as np
+from quimb.tensor.tensor_core import rand_uuid
+import torch
+import os, sys
+storedir = '/home/matt/labcode/quimb_testing'
+
+class Tensordot(torch.autograd.Function):
+    @staticmethod
+    def forward(self, A, B, axes, to_disk):
+        C = torch.tensordot(A, B, dims=axes)
+        self.to_disk = to_disk
+        self.axes = axes
+
+        if not self.to_disk:
+            self.save_for_backward(A, B)
+        else:
+            st = rand_uuid()
+            torch.save(A, os.path.join(storedir, f'store/tdot/A{st}.pt'))
+            torch.save(B, os.path.join(storedir, f'store/tdot/B{st}.pt'))
+            self.identify = st
+
+        return C
+
+    @staticmethod
+    def backward(self, dC):
+        if not self.to_disk:
+            A, B = self.saved_tensors
+        else:
+            st = self.identify
+            A = torch.load(os.path.join(storedir, f'store/tdot/A{st}.pt'))
+            B = torch.load(os.path.join(storedir, f'store/tdot/B{st}.pt'))
+
+        axes = self.axes
+        A_ndim = len(A.size())
+        B_ndim = len(B.size())
+
+        # first do the adjoint of A
+        if B_ndim == 0:
+            dA = dC * B
+
+        else:
+            dC_axes = list(np.arange(len(dC.size())))
+            if type(axes) is int:
+                axes = max(axes, 0)
+                B_axes = list(np.arange(B_ndim))
+                dA = torch.tensordot(dC, B, dims=[dC_axes[A_ndim-axes:], B_axes[axes:]])
+            else:
+                axes0 = [axes[0]] if type(axes[0]) is int else axes[0]
+                axes1 = [axes[1]] if type(axes[1]) is int else axes[1]
+                axes = [axes0, axes1]
+                A_axes = np.arange(A_ndim)
+                B_axes = np.arange(B_ndim)
+                summed_axes = [np.asarray(axes[0], dtype='int64') % A_ndim,
+                               np.asarray(axes[1], dtype='int64') % B_ndim]
+                other_axes  = [np.delete(A_axes, summed_axes[0]),
+                               np.delete(B_axes, summed_axes[1])]
+                perm = np.argsort(np.concatenate(
+                        (other_axes[0], summed_axes[0][np.argsort(summed_axes[1])])))
+                out = torch.tensordot(dC, B, dims=[dC_axes[len(other_axes[0]):],
+                                                                list(other_axes[1])])
+                dA = out.permute(list(perm))
+
+        # now do the adjoint of B
+        if A_ndim == 0:
+            dB = dC * A
+
+        else:
+            dC_axes = list(np.arange(len(dC.size())))
+            if type(axes) is int:
+                axes = max(axes, 0)
+                A_axes = list(np.arange(A_ndim))
+                dB = torch.tensordot(A, dC, dims=[A_axes[:A_ndim-axes],
+                                                        dC_axes[:A_ndim-axes]])
+            else:
+                axes0 = [axes[0]] if type(axes[0]) is int else axes[0]
+                axes1 = [axes[1]] if type(axes[1]) is int else axes[1]
+                axes = [axes0, axes1]
+                A_axes = np.arange(A_ndim)
+                B_axes = np.arange(B_ndim)
+                summed_axes = [np.asarray(axes[0], dtype='int64') % A_ndim,
+                               np.asarray(axes[1], dtype='int64') % B_ndim]
+                other_axes  = [np.delete(A_axes, summed_axes[0]),
+                               np.delete(B_axes, summed_axes[1])]
+                perm = np.argsort(np.concatenate(
+                    (summed_axes[1][np.argsort(summed_axes[0])], other_axes[1])))
+                out = torch.tensordot(A, dC, dims=[list(other_axes[0]),
+                                                    dC_axes[:len(other_axes[0])]])
+                dB = out.permute(list(perm))
+
+        return dA, dB, None, None
+
+
+def test_tdot():
+    L, M, N = 5, 40, 10
+    torch.manual_seed(2)
+    t1 = torch.rand(M, N, L, dtype=torch.float64, requires_grad=True)
+    t2 = torch.rand(M, L, N, dtype=torch.float64, requires_grad=True)
+    assert(torch.autograd.gradcheck(Tensordot.apply, (t1, t2, [[0,1],[0,2]], True), eps=1e-6, atol=1e-4))
+    print("Test Pass!")
+
+if __name__=='__main__':
+    test_tdot()
